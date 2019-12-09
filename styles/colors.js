300d28d41cc43c1e9ac77e6456cea8c9e1a11ce0
kolbeinsson <steinthor.kolbeinsson@bouvet.no>
2019-12-09 13:50:40 +0100

    Adding global colors

---
+++ styles/colors.js
@@ -0,0 +1,17 @@
+export const COLOR = {
+
+    BLUE_LIGHT: '#00A8b7',
+    BLUE_DARK: '#0059B2',
+    BLUE_VERY_DARK: '#24515E',
+
+    GREEN_MEDIUM: '#007C42',
+    GREEN_DARK: '#00563B',
+    GREEN_VERY_DARK: '#0070C',
+
+    GREY_DARK: '#F1F1F1',
+    
+    BROWN_DARK: '#3D2415',
+
+    BLACK: '#2B2B2B'
+
+};
