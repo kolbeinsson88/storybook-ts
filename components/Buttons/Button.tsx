66c3c72a00fc7ff4b68eb2960fd835e8965f143a
kolbeinsson <steinthor.kolbeinsson@bouvet.no>
2019-12-09 12:34:01 +0100

    Button first draft.

--- components/Buttons/Button.tsx
+++ components/Buttons/Button.tsx
@@ -1,19 +1,20 @@
 import React, { FunctionComponent } from 'react';
-import styled from 'styled-components';
+import styled from '@emotion/styled';
 
 import { FaGgCircle } from 'react-icons/fa';
-
 
 type Props = {
     color: string,
     disabled: boolean,
     fullWidth: boolean,
-    href: any,
-    target: any,
-    loading: boolean,
     size: string,
-    variant: string
+    href?: any,
+    target?: any,
+    loading?: boolean,
+    variant?: string
 };
+
+// const Base = ``;
 
 const PrimaryButton = styled.button<Props>`
     color: ${(props: Props) => props.color};
@@ -49,21 +50,27 @@ const PrimaryButton = styled.button<Props>`
         top: 32%;
         left: 15%;
     }
+
+    a {
+        color: ${props => props.color};
+        text-decoration: none;   
+    }
 `;
 
 const TertiaryButton = styled.button<Props>`
-    color: ${(props: Props) => props.color};
+
+    width: ${(props: Props) => props.fullWidth ? "100%" : "128px"};
+    color: ${(props: Props) => props.color};    
+
     background-color: #fff;
-    padding: 8px 32px;
+    padding: ${(props: Props) => props.size === "small" ?  8 : 12}px;
 
     border: 1px solid black;
     border-radius: 16px;
     font-weight: 600;
 
     border-radius: 16px;
-    padding: ${(props: Props) => props.size === "small" ?  8 : 12}px;
-    width: ${(props: Props) => props.fullWidth ? "100%" : "128px"};
-    
+
     cursor: pointer;
 
     :hover {
@@ -87,17 +94,30 @@ const TertiaryButton = styled.button<Props>`
         top: 32%;
         left: 15%;
     }
+
+    a {
+        color: ${props => props.color};
+        text-decoration: none;
+    }
 `;
 
 const Button: FunctionComponent<Props> = ({children, color, disabled, fullWidth, href, target, loading, size, variant }) => {
     if (variant === 'primary') {
         if (loading) {
-            return <PrimaryButton color={color} disabled={disabled} fullWidth={fullWidth} size={size}><span><FaGgCircle color="white" /></span>{children}</PrimaryButton>;
+                return (<PrimaryButton color={color} disabled={disabled} fullWidth={fullWidth} size={size}>
+                    {href ? <><span><FaGgCircle color="white" /></span><a href={target ? target : "#"} >{children}</a></>
+                          : <><span><FaGgCircle color="white" /></span>{children}</>
+                    }
+                </PrimaryButton>);
         }
         return <PrimaryButton color={color} disabled={disabled} fullWidth={fullWidth} size={size}>{children}</PrimaryButton>;
     } else if (variant === 'tertiary') {
         if(loading) {
-            return <TertiaryButton color={color} disabled={disabled} fullWidth={fullWidth} size={size}><span><FaGgCircle color="red" /></span>{children}</TertiaryButton>;
+            return <TertiaryButton color={color} disabled={disabled} fullWidth={fullWidth} size={size}>
+                {href ? <><span><FaGgCircle color="red" /></span><a href={target ? target : "#"} >{children}</a></>
+                    : <><span><FaGgCircle color="red" /></span>{children}</>
+                }
+            </TertiaryButton>;
         }
         return <TertiaryButton color={color} disabled={disabled} fullWidth={fullWidth} size={size}>{children}</TertiaryButton>;
     }
